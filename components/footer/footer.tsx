ab19d96a8e5778f5790a9e09e8ef386da2808145
Piotr Rudnicki <prudnicki@plog.pl>
2024-07-02 08:52:31 +0200

    Update footer.tsx

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-16 bg-sea dark:bg-gray-dark dark:border-t py-6">
       <div className="max-w-7xl mx-4 lg:mx-auto flex justify-between">
-        <img src="/exadel.svg" className="w-20 md:w-32" />
+        <img src="/exadel.svg" className="w-20 md:w-32" alt="" />
         <div className="flex flex-col md:flex-row w-2/3 justify-around items-end">
           <a
             href="#"
